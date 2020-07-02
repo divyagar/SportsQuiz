ed7c08ebdb256387103218e4bac5526311c10fe2
divyagar <42246254+divyagar@users.noreply.github.com>
2020-07-02 19:07:24 +0530

    Header file of functions.cpp

---
+++ functions.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTIONS
+    #define FUNCTIONS
+    
+    void mcqRules(void);
+    void oneWordRules(void);
+    void oneWordQuestions(void);
+    void mcqQuestions(void);
+    void mcqQuestionsMain(void);
+    void oneWordQuestionsMain(void);
+    void getInfos(ifstream *, ifstream *);
+    void openFiles(ifstream *, ifstream *, int *, int *);
+    int displayRules(string *);
+    void doesUserExists(string *);
+    void addUsers(string *, int);
+    void showFeedback(int);
+    // int askRandomQuestion(int);
+
+#endif
